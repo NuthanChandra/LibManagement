d3819809ef6a9347527a46d14f2a6c2f5648df2f
NuthanChandra <nuthanchandra1997@gmail.com>
2019-08-21 00:06:58 +0530

    Added date login in js file

--- public/javascripts/my_js.js
+++ public/javascripts/my_js.js
@@ -1,1 +1,6 @@
-console.log("hi")
+$("#issue_date").change(function() {
+    var date = new Date(this.value);
+    date.setDate(date.getDate() + 15);
+    // Further extract the Day and add or delete a day
+    $("#return_date").val(date.toISOString().split('T')[0]);
+  });
